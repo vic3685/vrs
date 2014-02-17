646647f77885111dad1301d0e301f53dc5747657
vic3685 <victoria.ruiz.schulze@gmail.com>
2014-02-17 13:31:54 -0300

    Use pushState if HTML5 history is supported
    Progressive enhancement. If history is not supported, regular links will
    work just fine.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,3 +2,46 @@
    Use requirejs (http://requirejs.org/) to include more JS files.
    You don't need to include requirejs yourself!
    The Grunt scripts will automatically merge all these files into /compiled/scripts.js */
+   
+  
+$(function() {
+
+	if (Modernizr.history) { // history is supported; 
+		$(".thumbnails").delegate("a", "click", function() { 
+			_href = $(this).attr("href");
+			console.log(_href);
+			history.pushState(null, null, _href); // change the url without a page refresh and add a history entry.
+			loadContent(_href); // load the content
+			return false;
+		}); 
+		$(window).bind("popstate", function() { 
+			link = location.pathname.replace(/^.*[\\/]/, ""); // get filename only
+			loadContent(link);
+		});
+	} 
+
+	var $mainContent = $("#mainImage"),
+		$contentInfo = $(".album_photos .info"),
+		$el;
+
+	function loadContent(href) {
+		console.log("loading");
+		$mainContent
+			.find("img")
+			.fadeOut(200, function() { // fade out the content of the current page
+				$mainContent
+				//.hide()
+				.load(href + " #mainImage img", function() {	
+					$(".thumbnails a").removeClass("current");
+					$(".thumbnails a[href$='" + href + "']").addClass("current");
+				});
+			});	
+		$(".itemInfo")
+			.fadeOut(200, function() {
+				console.log("new attempt started");
+				$(".itemInfo").load(href + " .itemInfo > *", function() {$(this).fadeIn(200);console.log("new attempt over");});
+				
+			});
+	};
+
+});
